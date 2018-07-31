dfbf98bb2de85ec655a6d38eb33542369a087c9a
Lea Ellingwood <leaellingwood@hotmail.com>
2018-07-31 12:19:40 -0400

    first commit

---
+++ mycode.js
@@ -0,0 +1,65 @@
+// kata 1
+console.log("Display the numbers from 1 to 20.");
+for (let i = 1; i < 21; i++) {
+    console.log(i);
+}
+
+// kata 2
+console.log("Display the even numbers from 1 to 20.")
+for (let i = 2; i < 21; i+=2) {
+    console.log(i);
+}
+
+//  kata 3
+console.log("Display the odd numbers from 1 to 20.")
+for (let i = 1; i <21; i+=2) {
+    console.log(i);
+}
+
+// kata 4
+console.log("Display the multiples of 5 up to 100.")
+for (let i = 0; i < 101; i+=5) {
+    console.log(i);
+}
+
+// kata 5
+console.log("Display the square numbers up to 100.")
+// let kata5 = 0;
+// while (kata5 < 11) {
+//     console.log(i);
+//     kata5= (i * i);
+//     kata5++;
+// }
+
+for (let i = 0; i > 11; i++) {
+    console.log(i);
+    i = i * i;
+    
+}
+
+// kata 6
+console.log("Display the numbers counting backwards from 20.")
+for (let i = 20; i > 0; i--) {
+    console.log(i);
+}
+
+// kata 7
+console.log("Display the even numbers counting backwards from 20.")
+for (let i = 20; i > 0; i-=2) {
+    console.log(i);
+}
+
+// kata 8
+console.log("Diplay the odd numbers from 20 to 1, counting backwards.")
+for (let i = 19; i > 0; i-=2) {
+    console.log(i);
+}
+
+// kata 9
+console.log("Display the multiples of 5, counting down from 100.")
+for (let i = 100; i > 0; i-=5) {
+    console.log(i);
+}
+
+// kata 10
+console.log("Display the square numbers, counting down from 100.")
